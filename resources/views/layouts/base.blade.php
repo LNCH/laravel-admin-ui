98e0a5e1729759b8075cc0a92edca16e5d2de8b5
Tom Lynch <tom@lnch.co.uk>
2021-10-23 19:24:08 +0100

    Add stacks and initial Google fonts

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,14 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        @isset($attributes['page-title'])
-            <title>{{ $attributes['page-title'] }}</title>
+        @hasSection('page-title')
+            <title>@yield('page-title')</title>
         @else
-            <title>{{ config('app.name', 'NuBrite') }}</title>
+            <title>{{ config('app.name', 'Laravel Admin UI') }}</title>
         @endif
 
         <!-- Fonts -->
         @stack('head_fonts')
+
+        <!-- Web Fonts -->
+        @if (count(config('laravel-admin-ui.google_fonts', [])) > 1)
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            @foreach (config('laravel-admin-ui.google_fonts', []) as $font)
+                <link href="{{ $font }}" rel="stylesheet">
+            @endforeach
+        @endif
 
         <!-- Styles -->
         @stack('head_styles')
@@ -20,8 +29,8 @@
 
     <body class="{{ implode(' ', array_merge($bodyClasses ?? [], ['font-sans', 'antialiased'])) }}">
         @yield('body')
-    </body>
 
-    <!-- Scripts -->
-    @stack('body_scripts')
+        <!-- Scripts -->
+        @stack('body_scripts')
+    </body>
 </html>
