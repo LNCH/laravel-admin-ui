8554f1c869616397a875c19b9f2198e78fab1609
Tom Lynch <tom@lnch.co.uk>
2021-10-23 23:24:24 +0100

    Add user icon and basic styling for account dropdown

--- resources/views/layouts/themes/default.blade.php
+++ resources/views/layouts/themes/default.blade.php
@@ -30,7 +30,11 @@
 
             {{-- Account Dropdown --}}
             <div class="account-dropdown">
-                <button class="header-button">AD</button>
+                <button class="header-button">
+                    <span class="sr-only">Account Menu</span>
+                    <span class="user-icon"></span>
+                    <span class="user-name">Tom Lynch</span>
+                </button>
             </div>
         </header>
 
