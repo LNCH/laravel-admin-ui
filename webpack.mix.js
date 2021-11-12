8220e2be20409e997421131883bec72bac7c871e
Tom Lynch <tom@lnch.co.uk>
2021-11-12 13:53:44 +0000

    Add in an images directory with some base images

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,5 @@ let mix = require('laravel-mix');
 mix.sass('resources/sass/app.scss', 'dist/css');
 
 mix.js('resources/js/app.js', 'dist/js');
+
+mix.copyDirectory('resources/images', 'dist/images');
